04bd3cdf929829aa6a31c537f24a300b7064c5c3
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-06 20:42:20 +0800

    add diregram initial files with vuex and material ui

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -2,7 +2,6 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
           &#8220;First, solve the problem. Then, write the code.&#8221;
           <footer>
@@ -16,29 +15,27 @@
   </v-container>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script lang="ts">
+  export default {
+    name: 'HelloWorld',
+    props: {}
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import '@babel/polyfill';
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/';
+import Api from './services/api';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+// initialize api service
+Api.init();
 
 new Vue({
   router,
