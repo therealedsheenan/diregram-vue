f6a6c4f5139f49ed6a22d04a94618b58ddcaccdb
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-16 02:08:42 +0800

    partial login component at home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <Login />
     <Timeline />
   </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator';
-  import Timeline from '@/components/Timeline.vue'; // @ is an alias to /src
+  import Timeline from '@/components/Timeline.vue';
+  import Login from '@/components/auth/Login.vue';
 
   @Component({
     components: {
       Timeline,
+      Login
     },
     data: () => {
       return {}
