83ca5e8b5e2168402aa36c2e289dc1f8181d925b
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-16 02:07:10 +0800

    main vuew initialization config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import '@babel/polyfill';
 import Vue from 'vue';
+import Axios from 'axios';
+
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
@@ -8,6 +10,12 @@ import Api from './services/api';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token');
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
 
 // initialize api service
 Api.init();
