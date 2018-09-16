b0f235c1e50a9343af553e2b8f2ff849d2f44d97
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-16 15:06:27 +0800

    add signup and login authentication components

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,21 +1,32 @@
 <template>
-  <v-container grid-list-xs>
-    <v-layout row wrap>
-      <v-flex xs10 offset-xs1>
-        <form class="login" @submit.prevent="requestLogin">
-          <h1>Login</h1>
-          <input required v-model="email" type="email" placeholder="Email"/>
-          <input required v-model="password" type="password" placeholder="Password"/>
-          <hr/>
-          <button type="submit">Login</button>
-        </form>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-card>
+    <v-card-title>
+      <span class="headline">Login</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <form class="login" @submit.prevent="requestLogin">
+            <v-flex xs12>
+              <v-text-field v-model="email" label="Email" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
+            </v-flex>
+            <v-btn color="red darken-1" flat @click.native="$emit('dialog-cancel')">Cancel</v-btn>
+            <v-btn color="blue darken-1" type="submit" flat>Login</v-btn>
+          </form>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+  </v-card>
+
 </template>
 
 <script lang="ts">
   import { mapGetters } from 'vuex';
+  import { REQUEST_LOGIN } from '@/store/types/actions';
+
   export default {
     name: 'Login',
     data: () => ({
@@ -32,8 +43,11 @@
       requestLogin () {
         console.log('logging in');
         const { email, password } = this;
-        this.$store.dispatch('requestLogin', { email, password })
-          .then(() => this.$router.push('/about'))
+        this.$store.dispatch(REQUEST_LOGIN, { email, password })
+          .then(() => {
+            console.log('login success!');
+            // this.$router.push('/about')
+          })
           .catch((err) => console.log(err));
       },
     }
@@ -41,5 +55,8 @@
 </script>
 
 <style scoped lang="scss">
-
+  form {
+    display: block;
+    width: 100%;
+  }
 </style>
--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -1,27 +1,41 @@
 <template>
-  <v-container grid-list-xs>
-    <v-layout row wrap>
-      <v-flex xs10 offset-xs1>
-        <form class="login" @submit.prevent="requestSignup">
-          <h1>Sign up</h1>
-          <input required v-model="username" type="text" placeholder="Username" />
-          <input required v-model="password" type="password" placeholder="Password" />
-          <!--<input id="password-confirm" type="password" v-model="passwordConfirmation" required />-->
-          <button type="submit">Login</button>
-        </form>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-card>
+    <v-card-title>
+      <span class="headline">Sign up</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <form class="signup" @submit.prevent="requestSignup">
+            <v-flex xs12>
+              <v-text-field v-model="username" label="Username" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field v-model="email" label="Email" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
+            </v-flex>
+            <v-btn color="red darken-1" flat @click.native="$emit('dialog-cancel')">Cancel</v-btn>
+            <v-btn color="blue darken-1" type="submit" flat>Sign up</v-btn>
+          </form>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+  </v-card>
+
 </template>
 
 <script lang="ts">
   import { mapGetters } from 'vuex';
+  import { REQUEST_SIGNUP } from '@/store/types/actions';
+
   export default {
     name: 'Signup',
     data: () => ({
       username: '',
-      password: '',
-      // passwordConfirmation: ''
+      email: '',
+      password: ''
     }),
     mounted () {
     },
@@ -31,19 +45,22 @@
     },
     methods: {
       requestSignup () {
-        // const { username, password, passwordConfirmation } = this;
-        // this.$store.dispatch('requestLogin', {
-        // username,
-        // password,
-        // });
-        // .then(() => this.$router.push('/about'))
-        // .catch((err) => console.log(err));
-        console.log('test')
+        console.log('Signing up');
+        const { username, email, password } = this;
+        this.$store.dispatch(REQUEST_SIGNUP, { username, email, password })
+          .then(() => {
+            console.log('login success!');
+            this.$router.push('/about');
+          })
+          .catch((err) => console.log(err));
       },
     }
   }
 </script>
 
 <style scoped lang="scss">
-
+  form {
+    display: block;
+    width: 100%;
+  }
 </style>
