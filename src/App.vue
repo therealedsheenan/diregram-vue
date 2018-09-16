9135f111c272586d83541048ffae31398423ef01
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-16 15:06:43 +0800

    app initialization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar>
-      <div class="navigation">
-        <router-link to="/">
-          <span class="navigation-logo">
-            <img src="@/assets/logo.png" alt="diregram">
-          </span>
-          <v-toolbar-title v-text="title"></v-toolbar-title>
-        </router-link>
-        <span v-if="isAuthed"> | <a @click="logout">Logout</a></span>
-        <!--<router-link to="/about">about</router-link>-->
-      </div>
-    </v-toolbar>
+    <Navigation />
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -19,56 +8,19 @@
 </template>
 
 <script lang="ts">
-  import { mapGetters } from 'vuex';
   import Timeline from './components/Timeline'
+  import Navigation from './components/Navigation';
 
   export default {
     name: 'App',
     components: {
-      Timeline
+      Timeline,
+      Navigation
     },
     data: () => {
       return {
         title: 'Diregram'
       }
     },
-    computed: {
-      ...mapGetters([
-        'isAuthed'
-      ])
-    },
-    methods: {
-      logout: () => {
-        console.log('logout!');
-        // this.$store.dispatch('logout')
-        //   .then(() => {
-        //     this.$router.push('/login')
-        //   })
-      }
-    },
   }
 </script>
-
-<style scoped lang="scss">
-  .navigation {
-    > a {
-      display: inline-block;
-      height: 30px;
-    }
-
-    &-logo {
-      width: 30px;
-      height: 30px;
-      display: inline-block;
-      img {
-        width: 100%;
-        height: auto;
-      }
-    }
-
-    .v-toolbar__title:not(:first-child) {
-      display: inline-block;
-      margin-left: 10px;
-    }
-  }
-</style>
